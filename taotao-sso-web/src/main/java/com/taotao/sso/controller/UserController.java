dc3ccf096ac006a737cf5db2c10e76f4340ee440
username <1037370201@qq.com>
2018-07-18 11:50:47 +0800

    单点登陆页面展示

--- taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
+++ taotao-sso-web/src/main/java/com/taotao/sso/controller/UserController.java
@@ -53,7 +53,7 @@ public class UserController {
     public String getUserByToken(@PathVariable String token,String callback){
         TaotaoResult result = userService.getUserByToken(token);
         if (StringUtils.isNotBlank(callback)){
-            return "callback("+JsonUtils.objectToJson(result)+");";
+            return callback+"("+JsonUtils.objectToJson(result)+");";
         }
         return JsonUtils.objectToJson(result);
     }
